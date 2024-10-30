142692364a91e2f4188707c6f700b5e50db78d07
Iryna Yehorova <yehorova.dev@gmail.com>
2024-10-30 21:51:55 +0200

    fix: correct layout home page

--- app/_components/HomePage/AnimatedArrowButton.tsx
+++ app/_components/HomePage/AnimatedArrowButton.tsx
@@ -7,7 +7,7 @@ export function AnimatedArrowButton() {
   return (
     <Link className="" href={`${Links.Characters}`}>
       <motion.div
-        className="home-button fixed bottom-[20%] right-[15%] z-[100] rounded-xl p-[2px] text-xl text-accent scale-[1]"
+        className="home-button fixed right-[50%] translate-x-1/2 bottom-[20%] md:right-[15%] md:translate-x-0 z-[100] rounded-xl p-[2px] text-xl text-accent scale-[1]"
         whileHover={{
           background:
             'conic-gradient(from 10deg, rgb(var(--background)), rgb(var(--foreground)), rgb(var(--accent)), rgb(var(--background)))',
@@ -20,7 +20,7 @@ export function AnimatedArrowButton() {
         }}
       >
         <div
-          className="rounded-xl bg-foreground py-6 pl-4 pr-6 text-xl text-accent"
+          className="rounded-xl bg-foreground p-3 pr-5 flex place-content-center place-items-center text-xl text-accent"
           style={{
             clipPath: 'polygon(84% 0, 0 0, 0 100%, 84% 100%, 100% 50%)',
           }}
